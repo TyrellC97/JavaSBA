314ae5690f1ef3f8475ebcca05eafe76d3579251
tyrellc97 <tyrellcoleman97@gmail.com>
2024-02-20 03:05:44 -0500

    student id and grade

--- Script.js
+++ Script.js
@@ -6,7 +6,7 @@
 const AssignmentGroup = {
   id: 50129,
   name: "java",
-  course_id: 501,
+  course_id: "101-b",
   group_weight: 100,
   assignments: [
     {
@@ -18,22 +18,65 @@ const AssignmentGroup = {
   ],
 };
 
-const LearnerSubmissions = [
+const learnerSubmissions = [
   {
-    learner_id: 501,
+    learnerId: 501,
     assignment_id: 25,
     submission: {
       submitted_at: 912,
       score: 25,
     },
   },
+  {
+    learnerId: 501,
+    assignment_id: 24,
+    submission: {
+      submitted_at: 913,
+      score: 45,
+    },
+  },
 ];
 
-const grade = LearnerSubmissions[0].submission.score;
+// console.log(learnerSubmissions[1].learnerID)
+
+const grade = learnerSubmissions[0].submission.score;
 const points = AssignmentGroup.assignments[0].points_possible;
 
 const finalScore = grade / points;
 
-// function getLearnerData()
-console.log("Student Number: " + LearnerSubmissions[0].learner_id);
-console.log("Assignment Score: " + finalScore);
+// function getLearnerData() {
+//   console.log("Student Number: " + LearnerSubmissions[0].learner_id);
+//   console.log("Assignment Score: " + finalScore);
+
+// }
+
+
+// function getLearnerData(){
+
+let students = {
+  student: 0,
+  grades: {
+
+  }
+}
+
+// Student Num
+  for ( i = 0; i < learnerSubmissions.length; i++)
+  if(learnerSubmissions[i].learnerId < 510){
+  students['student'] = learnerSubmissions[i].learnerId }
+
+  
+
+let grades
+// student grades 
+  for (i = 0; i < learnerSubmissions.length; i++)
+      if( learnerSubmissions[i].submission.score >= 0){
+        
+   students['grades'] = "Assign #" + learnerSubmissions[i].assignment_id + " - " + learnerSubmissions[i].submission.score +"/" + AssignmentGroup.assignments[0].points_possible
+
+    }
+
+console.log(students)
+
+
+// }
